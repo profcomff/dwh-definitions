00b6f343d855f85bf9435f9f362ff2ac9e6ceba5
antimoda1 <votamin444@mail.ru>
2023-09-01 19:08:09 +0300

    Added Social Class

--- profcomff_definitions/STG/__init__.py
+++ profcomff_definitions/STG/__init__.py
@@ -1,4 +1,4 @@
 from .union_member import UnionMember
+from .social import SocialActivity
 
-
-__all__ = ["UnionMember"]
+__all__ = ["UnionMember", "SocialActivity"]
---
+++ profcomff_definitions/STG/social.py
@@ -0,0 +1,11 @@
+import logging
+from sqlalchemy import Integer, String, JSON   
+from sqlalchemy.orm import Mapped, mapped_column
+from profcomff_definitions.base import Base
+logger = logging.getLogger(__name__)
+
+class SocialActivity(Base):
+	id: Mapped[int] = mapped_column(Integer, nullable=False, primary_key=True)
+	system: Mapped[String] = mapped_column(String, nullable=False)
+	message: Mapped[JSON] = mapped_column(JSON, nullable=False)
+
