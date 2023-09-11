5b7b16727a3ce85945e6a396a1be37f5924337cc
Wudext <jalchinn@yandex.ru>
2023-09-11 15:44:40 +0300

    Initial commit

--- profcomff_definitions/STG/__init__.py
+++ profcomff_definitions/STG/__init__.py
@@ -1,69 +1,15 @@
-from .auth import AuthMethod, Group, GroupScope, Scope, User, UserGroup, UserMessageDelay, UserSession, UserSessionScope
-from .marketing import ActionsInfo, User
-from .pinger import Alert, Fetcher, Metric, Receiver
-from .print import File, PrintFact, TgUser
-from .print import UnionMember as PrintUnionMember
-from .print import VkUser
-from .services import Button, Category
-from .services import Scope as ServicesScope
-from .social import VkGroups, WebhookStorage
-from .timetable import (
-    CommentEvent,
-    CommentLecturer,
-    Credentials,
-    Event,
-    EventsGroups,
-    EventsLecturers,
-    EventsRooms,
-    Group,
-    Lecturer,
-    Photo,
-    Room,
-)
-from .union_member import UnionMember
-from .userdata import Category, Info, Param, Source
+import os
+import sys
 
 
-__all__ = [
-    "UnionMember",
-    'AuthMethod',
-    'Group',
-    'GroupScope',
-    'Scope',
-    'User',
-    'UserGroup',
-    'UserMessageDelay',
-    'UserSession',
-    'UserSessionScope',
-    'ActionsInfo',
-    'User',
-    'Alert',
-    'Fetcher',
-    'Metric',
-    'Receiver',
-    'File',
-    'PrintFact',
-    'PrintUnionMember',
-    'Button',
-    'Category',
-    'ServicesScope',
-    'VkGroups',
-    'WebhookStorage',
-    'TgUser',
-    'CommentEvent',
-    'CommentLecturer',
-    'Credentials',
-    'Event',
-    'EventsGroups',
-    'EventsLecturers',
-    'EventsRooms',
-    'Group',
-    'Lecturer',
-    'Photo',
-    'Room',
-    'Category',
-    'Info',
-    'Param',
-    'Source',
-    'VkUser',
-]
+dir_path = os.path.dirname(os.path.abspath(__file__))
+files_in_dir = [f[:-3] for f in os.listdir(dir_path) if f.endswith('.py') and f != '__init__.py']
+for f in files_in_dir:
+    mod = __import__('.'.join([__name__, f]), fromlist=[f])
+    to_import = [getattr(mod, x) for x in dir(mod) if isinstance(getattr(mod, x), type)]
+
+    for i in to_import:
+        try:
+            setattr(sys.modules[__name__], i.__name__, i)
+        except AttributeError:
+            pass
