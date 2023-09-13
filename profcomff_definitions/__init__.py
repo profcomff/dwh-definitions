0f29079f805efdcc5332971c679a97fc0dc95556
Stanislav Roslavtsev <jalchinn@yandex.ru>
2023-09-13 18:39:56 +0300

    Fix

--- profcomff_definitions/__init__.py
+++ profcomff_definitions/__init__.py
@@ -1,4 +1,16 @@
-from .DM import *  # noqa
-from .DWH import *  # noqa
-from .ODS import *  # noqa
-from .STG import *  # noqa
+import os
+import sys
+
+
+dir_path = os.path.dirname(os.path.abspath(__file__))
+for folder in ['STG', 'DWH', 'DM', 'ODS']:
+    files_in_dir = [f[:-3] for f in os.listdir(dir_path + '/' + folder) if f.endswith('.py') and f != '__init__.py']
+    for f in files_in_dir:
+        mod = __import__('.'.join([__name__, folder, f]), fromlist=[f])
+        to_import = [getattr(mod, x) for x in dir(mod) if isinstance(getattr(mod, x), type)]
+
+        for i in to_import:
+            try:
+                setattr(sys.modules[__name__], i.__name__, i)
+            except AttributeError:
+                pass
