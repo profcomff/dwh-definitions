0b6866af24d5775a3cf57f5c9825f00f8135d544
Dyakov Roman <ROMAN@DYAKOV.SPACE>
2024-04-06 16:36:20 +0300

    Fixes in Makefile and Workflows (#27)

--- migrations/versions/fa6331fe4c72_rights.py
+++ migrations/versions/fa6331fe4c72_rights.py
@@ -5,6 +5,7 @@ Revises: 0a24041f09d1
 Create Date: 2023-08-28 13:35:51.998963
 
 """
+
 import os
 
 from alembic import op
@@ -32,41 +33,53 @@ def upgrade():
         "test_dwh_stg_union_member_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_stg_union_member_all"
     )
     op.grant_on_schema(
-        "test_dwh_stg_union_member_read"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_read",
+        (
+            "test_dwh_stg_union_member_read"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_read"
+        ),
         "STG_UNION_MEMBER",
     )
     op.grant_on_schema(
-        "test_dwh_stg_union_member_write"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_write",
+        (
+            "test_dwh_stg_union_member_write"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_write"
+        ),
         "STG_UNION_MEMBER",
     )
     op.grant_on_schema(
-        "test_dwh_stg_union_member_all"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_all",
+        (
+            "test_dwh_stg_union_member_all"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_all"
+        ),
         "STG_UNION_MEMBER",
     )
     op.grant_on_table(
-        "test_dwh_stg_union_member_read"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_read",
+        (
+            "test_dwh_stg_union_member_read"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_read"
+        ),
         ['SELECT'],
         '"STG_UNION_MEMBER".union_member',
     )
     op.grant_on_table(
-        "test_dwh_stg_union_member_write"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_write",
+        (
+            "test_dwh_stg_union_member_write"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_write"
+        ),
         ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
         '"STG_UNION_MEMBER".union_member',
     )
     op.grant_on_table(
-        "test_dwh_stg_union_member_all"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_all",
+        (
+            "test_dwh_stg_union_member_all"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_all"
+        ),
         ['ALL'],
         '"STG_UNION_MEMBER".union_member',
     )
@@ -74,42 +87,54 @@ def upgrade():
 
 def downgrade():
     op.revoke_on_table(
-        "test_dwh_stg_union_member_all"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_all",
+        (
+            "test_dwh_stg_union_member_all"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_all"
+        ),
         ['ALL'],
         '"STG_UNION_MEMBER".union_member',
     )
     op.revoke_on_table(
-        "test_dwh_stg_union_member_write"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_write",
+        (
+            "test_dwh_stg_union_member_write"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_write"
+        ),
         ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
         '"STG_UNION_MEMBER".union_member',
     )
     op.revoke_on_table(
-        "test_dwh_stg_union_member_read"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_read",
+        (
+            "test_dwh_stg_union_member_read"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_read"
+        ),
         ['SELECT'],
         '"STG_UNION_MEMBER".union_member',
     )
     op.revoke_on_schema(
-        "test_dwh_stg_union_member_all"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_all",
+        (
+            "test_dwh_stg_union_member_all"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_all"
+        ),
         "STG_UNION_MEMBER",
     )
     op.revoke_on_schema(
-        "test_dwh_stg_union_member_write"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_write",
+        (
+            "test_dwh_stg_union_member_write"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_write"
+        ),
         "STG_UNION_MEMBER",
     )
     op.revoke_on_schema(
-        "test_dwh_stg_union_member_read"
-        if os.getenv("ENVIRONMENT") != "production"
-        else "prod_dwh_stg_union_member_read",
+        (
+            "test_dwh_stg_union_member_read"
+            if os.getenv("ENVIRONMENT") != "production"
+            else "prod_dwh_stg_union_member_read"
+        ),
         "STG_UNION_MEMBER",
     )
     op.delete_group(
