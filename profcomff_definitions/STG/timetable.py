5c2030e80364600196024bcb6c02ed45a1d347b9
mixx3 <parfenov.ma21@physics.msu.ru>
2024-04-22 16:40:03 +0300

    fix migration

--- profcomff_definitions/STG/timetable.py
+++ profcomff_definitions/STG/timetable.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import JSON, Boolean, DateTime, Integer, String, Text, VARCHAR
+from sqlalchemy import JSON, VARCHAR, Boolean, DateTime, Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column
 
 from profcomff_definitions.base import Base
