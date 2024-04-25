5c2a6b03c70947bef5fdb56d4d1d027444e15b31
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2024-04-26 01:50:44 +0300

    Create __init__.py

---
+++ dwh/__init__.py
@@ -0,0 +1,14 @@
+from Authv import UserAuth, GroupAuth, UserGroupAuth, AuthMethodAuth, UserSessionAuth, ScopeAuth, GroupScopeAuth, UserSessionScopeAuth, UserMessageDelayAuth
+from Marketingv import UserM, ActionsInfoM
+from Physicsv import ContactsPh
+from Pingerv import ReceiverPi, AlertPi, FetcherPi, MetricPi
+from Printv import UnionMemberPr, FilePr
+from Servicesv import CategorySv, ButtonSv, ScopeSv
+from Socialv import WebhookStorageSoc, VkGroupsSoc
+from Timetablev import CredentialsT, RoomT, LecturerT, GroupT, EventT, EventsLecturersT, EventsRoomsT, EventsGroupsT, PhotoT, CommentEventT, CommentLecturerT
+from Union_memberv import UnionMemberUnimember
+from Userdatav import CategoryUd, ParamUd, SourceUd, InfoUd
+all = ["UserAuth", "GroupAuth","UserGroupAuth", "AuthMethodAuth", "UserSessionAuth", "ScopeAuth", "GroupScopeAuth", "UserSessionScopeAuth", "UserMessageDelayAuth","UserM", "ActionsInfoM", "ContactsPh",
+"ReceiverPi", "AlertPi", "FetcherPi", "MetricPi","UnionMemberPr", "FilePr","CategorySv", "ButtonSv", "ScopeSv","WebhookStorageSoc", "VkGroupsSoc",
+"CredentialsT", "RoomT", "LecturerT", "GroupT", "EventT", "EventsLecturersT", "EventsRoomsT", "EventsGroupsT", "PhotoT", "CommentEventT","CommentLecturerT",
+"UnionMemberUnimember", "CategoryUd", "ParamUd", "SourceUd", "InfoUd"]
