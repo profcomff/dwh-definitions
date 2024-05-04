a984d513694d914c8179bad52f63660a499069ff
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2024-05-04 18:03:42 +0300

    Update Printv.py

--- profcomff_definitions/ODS/validation/Printv.py
+++ profcomff_definitions/ODS/validation/Printv.py
@@ -1,14 +1,13 @@
 from pydantic import BaseModel, model_validator
-from sqlalchemy import Integer, Boolean, Column, String, DateTime
-from sqlalchemy.orm import mapped_column, Mapped
+from pydantic import int, str, Field, EmailStr, AnyURL, Json
 from datetime import datetime
 
-class UnionMemberPr(BaseModel):
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    surname: Mapped[str] = mapped_column(String)
-    union_number: Mapped[int] = mapped_column(Integer)
-    student_number: Mapped[int] = mapped_column(Integer)
 
+class UnionMemberPr(BaseModel):
+    id: int = Field(primary_key=True)
+    surname: str
+    union_number: int
+    student_number: int
     @model_validator(mode='before')
     def validate_card(self):
         id = self["id"]
@@ -37,14 +36,14 @@ class UnionMemberPr(BaseModel):
 
 
 class FilePr(BaseModel):
-    id: Mapped[int] = Column(Integer, primary_key=True)
-    pin: Mapped[str] = Column(String)
-    file: Mapped[str] = Column(String)
-    owner_id: Mapped[int] = Column(Integer)
-    option_pages: Mapped[str] = Column(String)
-    option_copies: Mapped[int] = Column(Integer)
-    option_two_sided: Mapped[bool] = Column(Boolean)
-    created_at: Mapped[datetime] = Column(DateTime)
-    updated_at: Mapped[datetime] = Column(DateTime)
-    number_of_pages: Mapped[int] = Column(Integer)
-    source: Mapped[str] = Column(String)
+    id: int = Field(primary_key=True)
+    pin: str
+    file: str
+    owner_id: int
+    option_pages: str
+    option_copies: int
+    option_two_sided: bool
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+    number_of_pages: int
+    source: str
