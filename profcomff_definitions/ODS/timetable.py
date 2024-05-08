113da413a2beeee6e588fcb76d30d63ed25dddd2
mixx3 <parfenov.ma21@physics.msu.ru>
2024-05-09 00:31:08 +0300

    initia; :

--- profcomff_definitions/ODS/timetable.py
+++ profcomff_definitions/ODS/timetable.py
@@ -2,18 +2,14 @@ from datetime import datetime
 
 from sqlalchemy import JSON, Boolean, DateTime, Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column
+import sqlalchemy as sa
 
 from profcomff_definitions.base import Base
 
 
 class OdsTimetableAct(Base):
-    event_text: Mapped[str] = mapped_column(String, nullable=True)
-    time_interval_text: Mapped[str] = mapped_column(String, nullable=True)
-    group_text: Mapped[str] = mapped_column(String, nullable=True)
+    event_text: Mapped[str | None] = mapped_column(String, nullable=True, index=True)
+    time_interval_text: Mapped[str | None] = mapped_column(String, nullable=True)
+    group_text: Mapped[str | None] = mapped_column(String, nullable=True)  
+    __mapper_args__ = {"primary_key": [event_text, time_interval_text, group_text]}  # Used only to correctly map ORM object to sql table
 
-    group: Mapped[str] = mapped_column(String)
-    email: Mapped[str] = mapped_column(String)
-    scope: Mapped[JSON] = mapped_column(JSON)
-    token: Mapped[JSON] = mapped_column(JSON)
-    create_ts: Mapped[datetime] = mapped_column(DateTime)
-    update_ts: Mapped[datetime] = mapped_column(DateTime)
