bd782016f7b1ae58efe2523d8f1a838e505c5567
mixx3 <parfenov.ma21@physics.msu.ru>
2024-05-09 00:34:40 +0300

    black

--- migrations/versions/20240509_0026_af10c6f8bb21_add_ods.py
+++ migrations/versions/20240509_0026_af10c6f8bb21_add_ods.py
@@ -6,9 +6,10 @@ Create Date: 2024-05-09 00:26:03.448401
 
 """
 
-from alembic import op
-import sqlalchemy as sa
 import os
+
+import sqlalchemy as sa
+from alembic import op
 
 
 revision = 'af10c6f8bb21'
--- profcomff_definitions/ODS/timetable.py
+++ profcomff_definitions/ODS/timetable.py
@@ -1,8 +1,5 @@
-from datetime import datetime
-
-from sqlalchemy import JSON, Boolean, DateTime, Integer, String, Text
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
-import sqlalchemy as sa
 
 from profcomff_definitions.base import Base
 
@@ -10,6 +7,7 @@ from profcomff_definitions.base import Base
 class OdsTimetableAct(Base):
     event_text: Mapped[str | None] = mapped_column(String, nullable=True, index=True)
     time_interval_text: Mapped[str | None] = mapped_column(String, nullable=True)
-    group_text: Mapped[str | None] = mapped_column(String, nullable=True)  
-    __mapper_args__ = {"primary_key": [event_text, time_interval_text, group_text]}  # Used only to correctly map ORM object to sql table
-
+    group_text: Mapped[str | None] = mapped_column(String, nullable=True)
+    __mapper_args__ = {
+        "primary_key": [event_text, time_interval_text, group_text]
+    }  # Used only to correctly map ORM object to sql table
