cf852079051d3e0b67b11272295dcd65cada4387
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2024-05-09 19:00:51 +0300

    Update Socialv.py

--- profcomff_definitions/ODS/validation/Socialv.py
+++ profcomff_definitions/ODS/validation/Socialv.py
@@ -5,15 +5,15 @@ from datetime import datetime
 
 #Social
 class WebhookStorageSoc(BaseModel):
-    id: int = Field(primary_key=True)
+    id: int 
     system: str 
     message: Json[Any]
 
 
 class VkGroupsSoc(BaseModel):
-    id: int = Field(primary_key=True)
+    id: int 
     group_id: int 
     confirmation_token: str
     secret_key: str
-    create_ts: datetime.datetime
-    update_ts: datetime.datetime
+    create_ts: datetime
+    update_ts: datetime
