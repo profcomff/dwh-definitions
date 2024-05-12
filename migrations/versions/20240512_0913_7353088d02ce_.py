c2612b79f9abef4a1f59109777d3f084791c0879
Zimovchik <63729114+Zimovchik@users.noreply.github.com>
2024-05-12 09:28:33 +0300

    channging down_revesion in migration (#48)

---
+++ migrations/versions/20240512_0913_7353088d02ce_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 7353088d02ce
+Revises: 8b6f4a99f692, d80ebb009b4e
+Create Date: 2024-05-12 09:13:24.853860
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import os
+
+
+# revision identifiers, used by Alembic.
+revision = '7353088d02ce'
+down_revision = ('8b6f4a99f692', 'd80ebb009b4e')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
