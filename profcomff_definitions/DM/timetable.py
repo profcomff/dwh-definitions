3cf5a60d2980f5cf4cebd7f29e986ac3f62c539e
Zimovchik <z4haroff.ivan@yandex.com>
2024-05-15 22:48:46 +0300

    dm_timetable schema

--- profcomff_definitions/DM/timetable.py
+++ profcomff_definitions/DM/timetable.py
@@ -10,3 +10,25 @@ class DimRoomAct(Base):
     room_direction_text_type: Mapped[str | None]
     is_deleted: Mapped[bool]
     department: Mapped[str | None]
+
+
+class DimLecturerAct(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    api_id: Mapped[int]
+    first_name: Mapped[str | None]
+    middle_name: Mapped[str | None]
+    last_name: Mapped[str | None]
+    avatar_id: Mapped[int | None]
+    description: Mapped[str | None]
+
+
+class DimGroupAct(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    api_id: Mapped[int]
+    name: Mapped[str | None]
+    number: Mapped[int | None]
+
+
+class DimClassAct(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str | None]
