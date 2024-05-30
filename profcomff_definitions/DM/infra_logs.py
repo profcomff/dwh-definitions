71a5c33e2df762cdeb11734e95e9d5c571e0d339
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2024-05-30 14:18:32 +0300

    Update infra_logs.py

--- profcomff_definitions/DM/infra_logs.py
+++ profcomff_definitions/DM/infra_logs.py
@@ -1,19 +1,6 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from profcomff_definitions.base import Base
 
-
-class ContainerLogCube(Base):
-    """Куб типов логов контейнера"""
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    container_name: Mapped[str] = mapped_column(comment="Название контейнера")
-    create_date: Mapped[str] = mapped_column(comment="Дата лога")
-    debug_cnt: Mapped[int] = mapped_column(comment="Количество записей с типом DEBUG")
-    warn_cnt: Mapped[int] = mapped_column(comment="Количество записей с типом WARN")
-    info_cnt: Mapped[int] = mapped_column(comment="Количество записей с типом INFO")
-    error_cnt: Mapped[int] = mapped_column(comment="Количество записей с типом ERROR")
-    critical_cnt: Mapped[int] = mapped_column(comment="Количество записей с типом CRITICAL")
-    other_cnt: Mapped[int] = mapped_column(comment="Количество записей с другими типами")
 class Incident(Base):
     """Информация об ошибках по контейнерам"""
     container_name: Mapped[str] = mapped_column(comment="Имя контейнера, в котором произошла ошибочка")
