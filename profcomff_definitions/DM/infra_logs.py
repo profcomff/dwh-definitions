544b3470030de5f751a78e90ac321111983ec7ce
SofyaFin <151746034+SofyaFin@users.noreply.github.com>
2024-05-31 23:38:33 +0300

    таблица DM_INFRA_LOGS

--- profcomff_definitions/DM/infra_logs.py
+++ profcomff_definitions/DM/infra_logs.py
@@ -1,6 +1,7 @@
 from sqlalchemy.orm import Mapped, mapped_column
 
 from profcomff_definitions.base import Base
+from datetime import datetime
 
 
 class ContainerLogCube(Base):
@@ -19,7 +20,7 @@ class ContainerLogCube(Base):
 
 class Incident(Base):
     """Информация об ошибках по контейнерам"""
-    id: Mapped[int] = Mappped[int] = mapped_column(comment="Айди")
+    id: Mapped[int] = mapped_column(primary_key=True)
     container_name: Mapped[str] = mapped_column(comment="Имя контейнера, в котором произошла ошибочка")
     message: Mapped[str] = mapped_column(comment="Сообщение об ошибке")
     create_ts: Mapped[datetime] = mapped_column(comment="Время, когда произошла ошибка")
