b2f63faa70ebd4be2fc62bae6e39245863d4f7dd
Zimovchik <63729114+Zimovchik@users.noreply.github.com>
2024-07-03 11:56:41 +0300

    dm_timetable schema добавил dim_lecturer_act, dim_group_act, dim_event_act (#50)
    ## Изменения
    <!-- Опишите здесь на языке, понятном каждому, изменения, сделанные в
    исходном коде по пунктам. -->
    
    ## Детали реализации
    <!-- Здесь можно описать технические детали по пунктам. -->
    
    ## Check-List
    <!-- После сохранения у следующих полей появятся галочки, которые нужно
    проставить мышкой -->
    - [x ] Вы проверили свой код перед отправкой запроса?
    - [x ] Вы написали тесты к реализованным функциям?
    - [ x] Вы не забыли применить форматирование `black` и `isort` для
    _Back-End_ или `Prettier` для _Front-End_?

--- profcomff_definitions/DM/timetable.py
+++ profcomff_definitions/DM/timetable.py
@@ -5,8 +5,30 @@ from profcomff_definitions.base import Base
 
 class DimRoomAct(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
-    api_id: Mapped[int]
-    name: Mapped[str | None]
+    room_api_id: Mapped[int]
+    room_name: Mapped[str | None]
     room_direction_text_type: Mapped[str | None]
-    is_deleted: Mapped[bool]
-    department: Mapped[str | None]
+    room_is_deleted: Mapped[bool]
+    room_department: Mapped[str | None]
+
+
+class DimLecturerAct(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    lecturer_api_id: Mapped[int]
+    lecturer_first_name: Mapped[str | None]
+    lecturer_middle_name: Mapped[str | None]
+    lecturer_last_name: Mapped[str | None]
+    lecturer_avatar_id: Mapped[int | None]
+    lecturer_description: Mapped[str | None]
+
+
+class DimGroupAct(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    group_api_id: Mapped[int]
+    group_name_text: Mapped[str | None]
+    group_number: Mapped[int | None]
+
+
+class DimEventAct(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    event_name_text: Mapped[str | None]
