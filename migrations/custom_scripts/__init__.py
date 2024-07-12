70c1ead928a16465822fd1bc1802b3e30c05602f
Stanislav Roslavtsev <jalchinn@yandex.ru>
2024-07-12 18:28:58 +0300

    Delete old logic

--- migrations/custom_scripts/__init__.py
+++ migrations/custom_scripts/__init__.py
@@ -1,7 +1,6 @@
 from .operations_groups import create_group, delete_group
 from .operations_schemas import create_table_schema, drop_table_schema
 from .render import render_create_group, render_create_sequence, render_drop_group, render_drop_sequence
-from .rights import compare_groups
 from .schemas import add_table_schema_to_model, compare_schemas
 from .tables import compare_table
 
@@ -17,5 +16,4 @@ __all__ = [
     'delete_group',
     'render_create_group',
     'render_drop_group',
-    'compare_groups',
 ]
