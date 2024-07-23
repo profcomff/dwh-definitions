96a3b877d7e2d4e8401cc81b022108fd01750ab5
RedstoneEnjoyer <moremath10@gmail.com>
2024-07-23 19:00:21 +0300

    class(table) info added + migration

---
+++ profcomff_definitions/ODS/info.py
@@ -0,0 +1,30 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from profcomff_definitions.base import Base
+
+class info(Base):
+    user_id: Mapped[int] = mapped_column(String, index=True, primary_key=True)
+    email: Mapped[str | None] = mapped_column(String, nullable=True)
+    phone_number: Mapped[str | None] = mapped_column(String, nullable=True)
+    vk_name: Mapped[str | None] = mapped_column(String, nullable=True)
+    city: Mapped[str | None] = mapped_column(String, nullable=True)
+    hometown: Mapped[str | None] = mapped_column(String, nullable=True)
+    location: Mapped[str | None] = mapped_column(String, nullable=True)
+    github_name: Mapped[str | None] = mapped_column(String, nullable=True)
+    telegram_name: Mapped[str | None] = mapped_column(String, nullable=True)
+    home_phone_number: Mapped[str | None] = mapped_column(String, nullable=True)
+    education_level: Mapped[str | None] = mapped_column(String, nullable=True)
+    university: Mapped[str | None] = mapped_column(String, nullable=True)
+    group: Mapped[str | None] = mapped_column(String, nullable=True)
+    faculty: Mapped[str | None] = mapped_column(String, nullable=True)
+    position: Mapped[str | None] = mapped_column(String, nullable=True)
+    student_id_number: Mapped[str | None] = mapped_column(String, nullable=True)
+    department: Mapped[str | None] = mapped_column(String, nullable=True)
+    mode_of_study: Mapped[str | None] = mapped_column(String, nullable=True)
+    full_name: Mapped[str | None] = mapped_column(String, nullable=True)
+    birth_date: Mapped[str | None] = mapped_column(String, nullable=True)
+    photo: Mapped[str | None] = mapped_column(String, nullable=True)
+    sex: Mapped[str | None] = mapped_column(String, nullable=True)
+    job: Mapped[str | None] = mapped_column(String, nullable=True)
+    work_location: Mapped[str | None] = mapped_column(String, nullable=True)
