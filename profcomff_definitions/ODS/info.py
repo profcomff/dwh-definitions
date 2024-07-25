3359bb812792f6e7e1da684e0575ec4c38a2eef9
RedstoneEnjoyer <moremath10@gmail.com>
2024-07-25 15:55:47 +0300

    Correction #1

--- profcomff_definitions/ODS/info.py
+++ profcomff_definitions/ODS/info.py
@@ -1,10 +1,10 @@
-from sqlalchemy import String
+from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from profcomff_definitions.base import Base
 
-class info(Base):
-    user_id: Mapped[int] = mapped_column(String, index=True, primary_key=True)
+class Info(Base):
+    user_id: Mapped[int] = mapped_column(Integer, index=True, primary_key=True)
     email: Mapped[str | None] = mapped_column(String, nullable=True)
     phone_number: Mapped[str | None] = mapped_column(String, nullable=True)
     vk_name: Mapped[str | None] = mapped_column(String, nullable=True)
