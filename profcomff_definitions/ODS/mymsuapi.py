bde4e0c44bfd7b91ae97f2487ccb64aca3fe8910
default <z4haroff.ivan@yandex.com>
2024-08-03 10:13:53 +0000

    migration message + lint

---
+++ profcomff_definitions/ODS/mymsuapi.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import TIMESTAMP
+from profcomff_definitions.base import Base
+from datetime import datetime
+
+
+class OdsTimetableApiFlattened(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    group_name: Mapped[str]
+    discipline_name: Mapped[str]
+    discipline_id: Mapped[int]
+    classroom_name: Mapped[str]
+    classroom_id: Mapped[int]
+    lesson_type_text: Mapped[str]
+    lesson_from_dttm_ts: Mapped[datetime]
+    lesson_to_dttm_ts: Mapped[datetime]
+    teacher_full_name: Mapped[str]
+    study_group_id: Mapped[int]
+    study_group_name: Mapped[str]
