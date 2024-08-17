c25df55a35531183ee19c27137b6ca4da9740620
default <samolko.mon@gmail.com>
2024-08-17 13:50:26 +0000

    adding 4 new tables, new migration

--- migrations/versions/20240815_1635_3b78055a2018_links.py
+++ migrations/versions/20240815_1635_3b78055a2018_links.py
@@ -6,9 +6,10 @@ Create Date: 2024-08-15 16:35:42.157636
 
 """
 
-from alembic import op
-import sqlalchemy as sa
 import os
+
+import sqlalchemy as sa
+from alembic import op
 
 
 # revision identifiers, used by Alembic.
--- profcomff_definitions/ODS/timetable.py
+++ profcomff_definitions/ODS/timetable.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer
+from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from profcomff_definitions.base import Base
