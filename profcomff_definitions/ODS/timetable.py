fb6ab4a0970aacb1b3061a75430ea0bb9c5ad717
MonTueVed <samolko.mon@gmail.com>
2024-08-17 16:01:04 +0300

    adding 4 new tables and migration

--- profcomff_definitions/ODS/timetable.py
+++ profcomff_definitions/ODS/timetable.py
@@ -11,3 +11,31 @@ class OdsTimetableAct(Base):
     __mapper_args__ = {
         "primary_key": [event_text, time_interval_text, group_text]
     }  # Used only to correctly map ORM object to sql table
+
+
+class OdsLinkTimetableTeacher(Base):
+    id: Mapped[int] = mapped_column(Integer, index=True, primary_key=True)
+    group: Mapped[str | None] = mapped_column(String, nullable=True)
+    event_tr: Mapped[str | None] = mapped_column(String, nullable=True)
+    teacher_id: Mapped[str | None] = mapped_column(Integer, nullable=True)
+
+
+class OdsLinkTimetableLesson(Base):
+    id: Mapped[int] = mapped_column(Integer, index=True, primary_key=True)
+    group: Mapped[str | None] = mapped_column(String, nullable=True)
+    event_tr: Mapped[str | None] = mapped_column(String, nullable=True)
+    lesson_id: Mapped[str | None] = mapped_column(Integer, nullable=True)
+
+
+class OdsLinkTimetableGroup(Base):
+    id: Mapped[int] = mapped_column(Integer, index=True, primary_key=True)
+    group: Mapped[str | None] = mapped_column(String, nullable=True)
+    event_tr: Mapped[str | None] = mapped_column(String, nullable=True)
+    group_id: Mapped[str | None] = mapped_column(Integer, nullable=True)
+
+
+class OdsLinkTimetableCabinet(Base):
+    id: Mapped[int] = mapped_column(Integer, index=True, primary_key=True)
+    group: Mapped[str | None] = mapped_column(String, nullable=True)
+    event_tr: Mapped[str | None] = mapped_column(String, nullable=True)
+    cabinet_id: Mapped[str | None] = mapped_column(Integer, nullable=True)
