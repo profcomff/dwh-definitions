c3a1d54e81ef6ce167a194a279a719a103411f03
Mikhail <87110754+mixx3@users.noreply.github.com>
2024-08-28 19:38:18 +0300

    initial (#73)
    ## Изменения
    <!-- Опишите здесь на языке, понятном каждому, изменения, сделанные в
    исходном коде по пунктам. -->
    
    ## Детали реализации
    <!-- Здесь можно описать технические детали по пунктам. -->
    
    ## Check-List
    <!-- После сохранения у следующих полей появятся галочки, которые нужно
    проставить мышкой -->
    - [ ] Вы проверили свой код перед отправкой запроса?
    - [ ] Вы написали тесты к реализованным функциям?
    - [ ] Вы не забыли применить форматирование `black` и `isort` для
    _Back-End_ или `Prettier` для _Front-End_?

--- profcomff_definitions/STG/rasphysmsu.py
+++ profcomff_definitions/STG/rasphysmsu.py
@@ -1,3 +1,6 @@
+import typing as tp
+
+from sqlalchemy import ARRAY, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from profcomff_definitions.base import Base
@@ -11,3 +14,18 @@ class RawHtml(Base):
 class RawHtmlOld(Base):
     url: Mapped[str] = mapped_column(primary_key=True)
     raw_html: Mapped[str | None]
+
+
+class Diff(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    subject: Mapped[str]
+    odd: Mapped[bool]
+    even: Mapped[bool]
+    weekday: Mapped[int | None]
+    num: Mapped[int | None]
+    start: Mapped[str]
+    end: Mapped[str]
+    place: Mapped[tp.List[int]] = mapped_column(ARRAY(Integer))
+    group: Mapped[tp.List[int]] = mapped_column(ARRAY(Integer))
+    teacher: Mapped[tp.List[int]] = mapped_column(ARRAY(Integer))
+    events_id: Mapped[tp.List[int]] = mapped_column(ARRAY(Integer))
