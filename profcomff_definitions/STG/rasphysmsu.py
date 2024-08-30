53d343a90c3f58c8c575a809735f31199e6d4170
default <parfenov.ma21@physics.msu.ru>
2024-08-30 17:01:23 +0000

    new_w_dates

--- profcomff_definitions/STG/rasphysmsu.py
+++ profcomff_definitions/STG/rasphysmsu.py
@@ -60,3 +60,18 @@ class New(Base):
     group: Mapped[tp.List[int]] = mapped_column(ARRAY(Integer))
     teacher: Mapped[tp.List[int]] = mapped_column(ARRAY(Integer))
     events_id: Mapped[tp.List[int] | None] = mapped_column(ARRAY(Integer))
+
+
+class NewWithDates(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    subject: Mapped[str]
+    odd: Mapped[bool]
+    even: Mapped[bool]
+    weekday: Mapped[int | None]
+    num: Mapped[int | None]
+    start: Mapped[str]
+    end: Mapped[str]
+    place: Mapped[tp.List[int]] = mapped_column(ARRAY(Integer))
+    group: Mapped[tp.List[int]] = mapped_column(ARRAY(Integer))
+    teacher: Mapped[tp.List[int]] = mapped_column(ARRAY(Integer))
+    events_id: Mapped[tp.List[int] | None] = mapped_column(ARRAY(Integer))
