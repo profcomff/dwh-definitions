eabe78ce541ec08b3ebfbec6ff108431a33dbc1a
default <z4haroff.ivan@yandex.com>
2024-10-06 07:31:41 +0000

    fix

---
+++ migrations/versions/20241006_0726_5c0a5eb043d7_button_is_hidden.py
@@ -0,0 +1,29 @@
+"""button_is_hidden
+
+Revision ID: 5c0a5eb043d7
+Revises: 0e97fd76b68a
+Create Date: 2024-10-06 07:26:54.197863
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import os
+
+
+# revision identifiers, used by Alembic.
+revision = '5c0a5eb043d7'
+down_revision = '0e97fd76b68a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('button', sa.Column('is_hidden', sa.Boolean(), nullable=False), schema='STG_SERVICES')
+    conn = op.get_bind()
+    conn.execute(sa.text(f"""UPDATE "STG_SERVICES".button SET "is_hidden"='false'"""))
+    op.alter_column('button', 'is_hidden', nullable=False, schema='STG_SERVICES')
+
+
+def downgrade():
+    op.drop_column('button', 'is_hidden', schema='STG_SERVICES')
