2320fb2e8ade1a62d83d35b6c91bed5b0d953d92
default <parfenov.ma21@physics.msu.ru>
2024-10-20 14:11:00 +0000

    init

---
+++ profcomff_definitions/STG/rating.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from uuid import UUID
+from sqlalchemy.orm import Mapped, mapped_column
+
+from profcomff_definitions.base import Base
+
+
+class Lecturer(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    middle_name: Mapped[str]
+    subject: Mapped[str| None]
+    avatar_link: Mapped[str| None]
+    timetable_id: Mapped[int]
+
+
+class Comment(Base):
+    uuid: Mapped[UUID] = mapped_column(primary_key=True)
+    create_ts: Mapped[datetime]
+    update_ts: Mapped[datetime]
+    subject: Mapped[str]
+    text: Mapped[str | None]
+    mark_kindness: Mapped[int]
+    mark_freebie: Mapped[int]
+    mark_clarity: Mapped[int]
+    lecturer_id: Mapped[int]
+    review_status: Mapped[str]
+
+
+class LecturerUserComment(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    lecturer_id: Mapped[int]
+    user_id: Mapped[int]
+    create_ts: Mapped[datetime]
+    update_ts: Mapped[datetime]
