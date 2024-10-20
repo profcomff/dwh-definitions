3f553fd04b7d21d7f508f31ee8c061f8fc3824d7
Mikhail <parfenov.ma21@physics.msu.ru>
2024-10-20 20:04:35 +0300

    fix (#81)

---
+++ migrations/versions/20241020_1700_8253b11f2c82_merge_heads.py
@@ -0,0 +1,27 @@
+"""merge_heads
+
+Revision ID: 8253b11f2c82
+Revises: 75df3cab6f2f, ee89b52980d8
+Create Date: 2024-10-20 17:00:48.622840
+
+"""
+
+import os
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '8253b11f2c82'
+down_revision = ('75df3cab6f2f', 'ee89b52980d8')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
