66142fb5eaee8fb0f922e409807242cd120c974d
default <parfenov.ma21@physics.msu.ru>
2024-10-20 15:17:55 +0000

    init

--- migrations/versions/20241020_1405_ee89b52980d8_add_rating_api_tables.py
+++ migrations/versions/20241020_1405_ee89b52980d8_add_rating_api_tables.py
@@ -6,9 +6,11 @@ Create Date: 2024-10-20 14:05:40.087805
 
 """
 
-from alembic import op
-import sqlalchemy as sa
 import os
+
+import sqlalchemy as sa
+from alembic import op
+
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
