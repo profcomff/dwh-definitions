3e7dd5ae9b1f617f77c0dea050a8c57cbe9bcb70
default <parfenov.ma21@physics.msu.ru>
2024-10-20 17:03:14 +0000

    format

--- migrations/versions/20241020_1700_8253b11f2c82_merge_heads.py
+++ migrations/versions/20241020_1700_8253b11f2c82_merge_heads.py
@@ -6,9 +6,10 @@ Create Date: 2024-10-20 17:00:48.622840
 
 """
 
-from alembic import op
-import sqlalchemy as sa
 import os
+
+import sqlalchemy as sa
+from alembic import op
 
 
 # revision identifiers, used by Alembic.
