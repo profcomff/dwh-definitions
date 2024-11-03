6e13dfcd5b9ad4d4c3fc8df3597f323f6302cbf4
default <parfenov.ma21@physics.msu.ru>
2024-11-03 11:16:13 +0000

    add scd2 tables

---
+++ migrations/versions/20241103_1114_cf7557c43271_scds_for_userdata.py
@@ -0,0 +1,120 @@
+"""scds for userdata
+
+Revision ID: cf7557c43271
+Revises: 8253b11f2c82
+Create Date: 2024-11-03 11:14:37.093594
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import os
+from sqlalchemy.dialects import postgresql
+
+# revision identifiers, used by Alembic.
+revision = 'cf7557c43271'
+down_revision = '8253b11f2c82'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'info_hist',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('param_id', sa.Integer(), nullable=True),
+        sa.Column('source_id', sa.Integer(), nullable=True),
+        sa.Column('owner_id', sa.Integer(), nullable=True),
+        sa.Column('value', sa.String(), nullable=True),
+        sa.Column('create_ts', sa.DateTime(), nullable=True),
+        sa.Column('modify_ts', sa.DateTime(), nullable=True),
+        sa.Column('is_deleted', sa.Boolean(), nullable=True),
+        sa.Column('valid_from_dt', sa.Date(), nullable=True),
+        sa.Column('valid_to_dt', sa.Date(), nullable=True),
+        sa.PrimaryKeyConstraint('id'),
+        schema='ODS_INFO',
+        comment='\n    SCD2 historical table based on STG_USERDATA.info\n    ',
+    )
+    op.create_table(
+        'param_hist',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.String(), nullable=True),
+        sa.Column('category_id', sa.Integer(), nullable=True),
+        sa.Column('is_required', sa.Boolean(), nullable=True),
+        sa.Column('changeable', sa.Boolean(), nullable=True),
+        sa.Column('type', sa.String(), nullable=True),
+        sa.Column('create_ts', sa.DateTime(), nullable=True),
+        sa.Column('modify_ts', sa.DateTime(), nullable=True),
+        sa.Column('is_deleted', sa.Boolean(), nullable=True),
+        sa.Column('validation', sa.String(), nullable=True),
+        sa.Column('valid_from_dt', sa.Date(), nullable=True),
+        sa.Column('valid_to_dt', sa.Date(), nullable=True),
+        sa.PrimaryKeyConstraint('id'),
+        schema='ODS_INFO',
+        comment='\n    SCD2 historical table based on STG_USERDATA.param\n    ',
+    )
+    op.grant_on_table(
+        "test_dwh_ods_info_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_read",
+        ['SELECT'],
+        '"ODS_INFO".info_hist',
+    )
+    op.grant_on_table(
+        "test_dwh_ods_info_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_write",
+        ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
+        '"ODS_INFO".info_hist',
+    )
+    op.grant_on_table(
+        "test_dwh_ods_info_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_all",
+        ['ALL'],
+        '"ODS_INFO".info_hist',
+    )
+    op.grant_on_table(
+        "test_dwh_ods_info_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_read",
+        ['SELECT'],
+        '"ODS_INFO".param_hist',
+    )
+    op.grant_on_table(
+        "test_dwh_ods_info_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_write",
+        ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
+        '"ODS_INFO".param_hist',
+    )
+    op.grant_on_table(
+        "test_dwh_ods_info_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_all",
+        ['ALL'],
+        '"ODS_INFO".param_hist',
+    )
+
+
+def downgrade():
+    op.revoke_on_table(
+        "test_dwh_ods_info_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_all",
+        ['ALL'],
+        '"ODS_INFO".param_hist',
+    )
+    op.revoke_on_table(
+        "test_dwh_ods_info_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_write",
+        ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
+        '"ODS_INFO".param_hist',
+    )
+    op.revoke_on_table(
+        "test_dwh_ods_info_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_read",
+        ['SELECT'],
+        '"ODS_INFO".param_hist',
+    )
+    op.revoke_on_table(
+        "test_dwh_ods_info_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_all",
+        ['ALL'],
+        '"ODS_INFO".info_hist',
+    )
+    op.revoke_on_table(
+        "test_dwh_ods_info_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_write",
+        ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
+        '"ODS_INFO".info_hist',
+    )
+    op.revoke_on_table(
+        "test_dwh_ods_info_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_ods_info_read",
+        ['SELECT'],
+        '"ODS_INFO".info_hist',
+    )
+    op.drop_table('param_hist', schema='ODS_INFO')
+    op.drop_table('info_hist', schema='ODS_INFO')
--- profcomff_definitions/ODS/info.py
+++ profcomff_definitions/ODS/info.py
@@ -1,3 +1,4 @@
+from datetime import datetime, date
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -29,3 +30,37 @@ class Info(Base):
     sex: Mapped[str | None] = mapped_column(String, nullable=True)
     job: Mapped[str | None] = mapped_column(String, nullable=True)
     work_location: Mapped[str | None] = mapped_column(String, nullable=True)
+
+
+class ParamHist(Base):
+    """
+    SCD2 historical table based on STG_USERDATA.param
+    """
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str | None]
+    category_id: Mapped[int | None]
+    is_required: Mapped[bool | None]
+    changeable: Mapped[bool | None]
+    type: Mapped[str | None]
+    create_ts: Mapped[datetime | None]
+    modify_ts: Mapped[datetime | None]
+    is_deleted: Mapped[bool | None]
+    validation: Mapped[str | None]
+    valid_from_dt: Mapped[date | None]
+    valid_to_dt: Mapped[date | None]
+
+
+class InfoHist(Base):
+    """
+    SCD2 historical table based on STG_USERDATA.info
+    """
+    id: Mapped[int] = mapped_column(primary_key=True)
+    param_id: Mapped[int | None]
+    source_id: Mapped[int | None]
+    owner_id: Mapped[int | None]
+    value: Mapped[str | None]
+    create_ts: Mapped[datetime | None]
+    modify_ts: Mapped[datetime | None]
+    is_deleted: Mapped[bool | None]
+    valid_from_dt: Mapped[date | None]
+    valid_to_dt: Mapped[date | None]
