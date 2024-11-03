e3aa16340550594ca0d90301b4cc180e10697e93
Mikhail <parfenov.ma21@physics.msu.ru>
2024-11-03 20:41:20 +0300

    Move to dwh (#88)

--- profcomff_definitions/ODS/auth.py
+++ profcomff_definitions/ODS/auth.py
@@ -9,19 +9,22 @@ class User(Base):
     """
     Historical table for STG_AUTH.user
     """
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int]
     is_deleted: Mapped[bool | None]
     create_ts: Mapped[datetime | None]
     update_ts: Mapped[datetime | None]
     valid_from_dt: Mapped[date | None]
     valid_to_dt: Mapped[date | None]
+    __mapper_args__ = {
+        "primary_key": ["id", "valid_from_dt"]
+    }  # Used only to correctly map ORM object to sql table
 
 
 class AuthMethod(Base):
     """
     Historical table for STG_AUTH.auth_method
     """
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int]
     user_id: Mapped[int | None]
     auth_method: Mapped[str | None]
     param: Mapped[str | None]
@@ -31,3 +34,6 @@ class AuthMethod(Base):
     is_deleted: Mapped[bool | None]
     valid_from_dt: Mapped[date | None]
     valid_to_dt: Mapped[date | None]
+    __mapper_args__ = {
+        "primary_key": ["id", "valid_from_dt"]
+    }  # Used only to correctly map ORM object to sql table
