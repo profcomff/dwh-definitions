967d516df3e1748f98b8d7f4e01199770f60ef06
default <z4haroff.ivan@yandex.com>
2024-11-10 13:24:04 +0000

    stg_union_member middle name add

--- profcomff_definitions/DM/monitoring.py
+++ profcomff_definitions/DM/monitoring.py
@@ -9,8 +9,11 @@ class DbMonitoringSnp(Base):
     """
     Snapshot table that shows sizes for all tables in DWH
     """
+
     id: Mapped[int]
-    table_name: Mapped[str] = mapped_column(comment="Table name with schema name, ex. \"STG_TIMETABLE\".\"events_groups\"")
+    table_name: Mapped[str] = mapped_column(
+        comment="Table name with schema name, ex. \"STG_TIMETABLE\".\"events_groups\""
+    )
     table_schema: Mapped[str] = mapped_column(comment="Table schema, need for detalization, ex.\"STG_TIMETABLE\"")
     table_size_mb: Mapped[int] = mapped_column(comment="Table size in MB (int), ex. 8")
     indexes_size_mb: Mapped[int] = mapped_column(comment="Table indexes size in MB(int), ex.5")
