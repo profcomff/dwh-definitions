3a0175d718202fe04c2dcbf28678c305174b1bfc
default <moremath10@gmail.com>
2024-11-10 10:55:55 +0000

    user_id -> id

--- profcomff_definitions/ODS/user.py
+++ profcomff_definitions/ODS/user.py
@@ -5,7 +5,7 @@ from profcomff_definitions.base import Base
 
 
 class Info(Base):
-    user_id: Mapped[int] = mapped_column(Integer, index=True, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, index=True, primary_key=True)
     email: Mapped[str | None] = mapped_column(String, nullable=True)
     auth_email: Mapped[str | None] = mapped_column(String, nullable=True)
     phone_number: Mapped[str | None] = mapped_column(String, nullable=True)
