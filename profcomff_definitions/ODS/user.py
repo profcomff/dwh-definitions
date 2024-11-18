89e2828e6380525dd7ea76c0c65afbd1ada9afc7
default <moremath10@gmail.com>
2024-11-18 13:55:48 +0000

    user_id to id in ODS.user

--- profcomff_definitions/ODS/user.py
+++ profcomff_definitions/ODS/user.py
@@ -1,13 +1,13 @@
 from datetime import date, datetime
 
-from sqlalchemy import Integer, String, Boolean
+from sqlalchemy import Integer, String, Booleanal
 from sqlalchemy.orm import Mapped, mapped_column
 
 from profcomff_definitions.base import Base
 
 
 class Info(Base):
-    user_id: Mapped[int] = mapped_column(Integer, index=True, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, index=True, primary_key=True)
     email: Mapped[str | None] = mapped_column(String, nullable=True)
     phone_number: Mapped[str | None] = mapped_column(String, nullable=True)
     vk_name: Mapped[str | None] = mapped_column(String, nullable=True)
