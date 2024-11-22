000b56d02fdb9958b22f551094c52bc949dd4b47
Stanislav Roslavtsev <jalchinn@yandex.ru>
2024-11-22 12:22:49 +0300

    Create readme + fix class checking (#103)

--- profcomff_definitions/__init__.py
+++ profcomff_definitions/__init__.py
@@ -6,13 +6,14 @@ __version__ = '1.0.0'  # Не менять, работает автоматик�
 
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
+tables = []
 for folder in ['STG', 'DWH', 'DM', 'ODS']:
     files_in_dir = [f[:-3] for f in os.listdir(dir_path + '/' + folder) if f.endswith('.py') and f != '__init__.py']
     for f in files_in_dir:
         mod = __import__('.'.join([__name__, folder, f]), fromlist=[f])
         to_import = [getattr(mod, x) for x in dir(mod) if isinstance(getattr(mod, x), type)]
-
         for i in to_import:
+            tables.append([i.__name__, i])
             try:
                 setattr(sys.modules[__name__], i.__name__, i)
             except AttributeError:
