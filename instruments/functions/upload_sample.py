d310863852531ad6cc0b7ae566fd8c467a2b6d5b
Stanislav Roslavtsev <jalchinn@yandex.ru>
2024-11-22 12:08:48 +0300

    Logging addition

--- instruments/functions/upload_sample.py
+++ instruments/functions/upload_sample.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from dotenv import load_dotenv
@@ -31,22 +32,23 @@ def upload_sample(table_class: str, limit: int, *args):
         local_session.execute(text('SELECT 1'))
         remote_session.execute(text('SELECT 1'))
 
-    except (OperationalError, ArgumentError, UnicodeDecodeError):
-        raise ConnectionError(
+    except (OperationalError, ArgumentError, UnicodeDecodeError) as e:
+        logging.error(
             "Could not connect to database. Perhaps your login/password/database for your postgres is incorrect. Please, check if you have .env file in root folder with correct database link"
         )
+        raise ConnectionError(e)
 
     # Собственно сама загрузка
     try:
         samples = remote_session.query(table_class).order_by(func.random()).limit(limit).all()
     except Exception as e:
-        print(e)
-        raise ConnectionError("Table does not exist in remote database. Please check migrations in remote database")
+        logging.error("Table does not exist in remote database. Please check migrations in remote database")
+        raise ConnectionError(e)
     try:
         for sample in samples:
             local_sampe = local_session.merge(sample)
             local_session.add(local_sampe)
             local_session.commit()
     except Exception as e:
-        print(e)
-        raise ConnectionError("Table does not exist in local database. Please check your migrations")
+        logging.error("Table does not exist in local database. Please check your migrations")
+        raise ConnectionError(e)
