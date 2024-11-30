e8ca81a7132284dcfb253ed9d13a018d48cbd5a2
Парфенов Михаил Александрович <parfenovma@it.profcomff.com>
2024-11-30 17:46:43 +0000

    addtables

---
+++ migrations/versions/20241130_1744_4662fc16ccde_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: 4662fc16ccde
+Revises: 09416a9fa7cc, d47cdc452c99
+Create Date: 2024-11-30 17:44:02.880484
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import os
+
+
+# revision identifiers, used by Alembic.
+revision = '4662fc16ccde'
+down_revision = ('09416a9fa7cc', 'd47cdc452c99')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
