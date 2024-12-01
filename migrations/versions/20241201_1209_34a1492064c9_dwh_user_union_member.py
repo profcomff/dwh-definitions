dc7ef4d9fbf66e38ebbbf45accbf6b6e60d17c1e
Финютина Софья Алексеевна <finyutinasa@it.profcomff.com>
2024-12-01 12:17:28 +0000

    isorted

--- migrations/versions/20241201_1209_34a1492064c9_dwh_user_union_member.py
+++ migrations/versions/20241201_1209_34a1492064c9_dwh_user_union_member.py
@@ -6,9 +6,10 @@ Create Date: 2024-12-01 12:09:12.184468
 
 """
 
-from alembic import op
-import sqlalchemy as sa
 import os
+
+import sqlalchemy as sa
+from alembic import op
 
 
 # revision identifiers, used by Alembic.
