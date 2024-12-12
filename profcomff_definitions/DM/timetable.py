9835d41fd813379adf51093783e0ace0727a1326
Парфенов Михаил Александрович <parfenovma@it.profcomff.com>
2024-12-12 15:46:40 +0000

    fixes

--- profcomff_definitions/DM/timetable.py
+++ profcomff_definitions/DM/timetable.py
@@ -1,10 +1,14 @@
+
+import uuid
+
+from sqlalchemy import UUID, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from profcomff_definitions.base import Base
 
 
 class DimRoomAct(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
     room_api_id: Mapped[int]
     room_name: Mapped[str | None]
     room_direction_text_type: Mapped[str | None]
@@ -13,7 +17,7 @@ class DimRoomAct(Base):
 
 
 class DimLecturerAct(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
     lecturer_api_id: Mapped[int]
     lecturer_first_name: Mapped[str | None]
     lecturer_middle_name: Mapped[str | None]
@@ -24,7 +28,7 @@ class DimLecturerAct(Base):
 
 
 class DimGroupAct(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
     group_api_id: Mapped[int]
     group_name_text: Mapped[str | None]
     group_number: Mapped[str | None]
@@ -32,7 +36,7 @@ class DimGroupAct(Base):
 
 
 class DimEventAct(Base):
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[uuid.UUID] = mapped_column(UUID, primary_key=True, default=uuid.uuid4)
     event_api_id: Mapped[int | None]
     event_name_text: Mapped[str | None]
     source_name: Mapped[str]
