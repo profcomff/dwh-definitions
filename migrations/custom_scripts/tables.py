62d8c19d8233a10266aaff3ee5f50bdb8cb32458
Nikita Timofeev <92633416+zipperman1@users.noreply.github.com>
2024-12-15 23:02:24 +0300

    Доработка dwh-definitons библиотеки (#118)
    ## Изменения
    Добавил SensitiveBase(Base), переписал кастомные скрипты для алембика
    
    ## Детали реализации
    см. выше
    
    ## Check-List
    - [x] Вы проверили свой код перед отправкой запроса?
    - [ ] Вы написали тесты к реализованным функциям?
    - [x] Вы не забыли применить форматирование `black` и `isort` для
    _Back-End_ или `Prettier` для _Front-End_?
    
    ---------
    
    Co-authored-by: Тимофеев Никита Алексеевич <timofeevna@it.profcomff.com>
    Co-authored-by: Mikhail <parfenov.ma21@physics.msu.ru>

--- migrations/custom_scripts/tables.py
+++ migrations/custom_scripts/tables.py
@@ -8,12 +8,14 @@ from .operations_tables import GrantRightsOp, RevokeRightsOp
 @comparators.dispatch_for("table")
 def compare_table(autogen_context, modify_table_ops, s, tname, metadata_table_db, metadata_table_code):
     if str(metadata_table_db) == 'None':
+        sensitive = metadata_table_code.info.get('sensitive', False)
         for render_scope in ['read', 'write', 'all']:
             group_name = (
-                f'test_dwh_{s}_{render_scope}'.lower()
+                f'test_{"sensitive_" if sensitive else ""}dwh_{s}_{render_scope}'.lower()
                 if os.getenv("ENVIRONMENT") != "production"
-                else f'prod_dwh_{s}_{render_scope}'.lower()
+                else f'prod_{"sensitive_" if sensitive else ""}dwh_{s}_{render_scope}'.lower()
             )
+            
             scopes = []
             match render_scope:
                 case 'read':
@@ -22,17 +24,23 @@ def compare_table(autogen_context, modify_table_ops, s, tname, metadata_table_db
                     scopes = ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT']
                 case 'all':
                     scopes = ['ALL']
-
+                    
+            if sensitive:
+                modify_table_ops.ops.append(
+                    CreateGroupOp(group_name=group_name)
+                )
+            
             modify_table_ops.ops.append(
                 GrantRightsOp(table_name=str(metadata_table_code), scopes=scopes, group_name=group_name)
             )
 
     elif str(metadata_table_code) == 'None':
+        sensitive = metadata_table_db.info.get('sensitive', False)
         for render_scope in ['read', 'write', 'all']:
             group_name = (
-                f'test_dwh_{s}_{render_scope}'.lower()
+                f'test_{"sensitive_" if sensitive else ""}dwh_{s}_{render_scope}'.lower()
                 if os.getenv("ENVIRONMENT") != "production"
-                else f'prod_dwh_{s}_{render_scope}'.lower()
+                else f'prod_{"sensitive_" if sensitive else ""}dwh_{s}_{render_scope}'.lower()
             )
             scopes = []
             match render_scope:
@@ -46,3 +54,8 @@ def compare_table(autogen_context, modify_table_ops, s, tname, metadata_table_db
             modify_table_ops.ops.append(
                 RevokeRightsOp(table_name=str(metadata_table_db), scopes=scopes, group_name=group_name)
             )
+            
+            if sensitive:
+                modify_table_ops.ops.append(
+                    DeleteGroupOp(group_name=group_name)
+                )
--- profcomff_definitions/base.py
+++ profcomff_definitions/base.py
@@ -2,6 +2,7 @@ import re
 
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import as_declarative
+from typing import Any
 
 from migrations.custom_scripts.schemas import add_table_schema_to_model
 
@@ -20,14 +21,29 @@ class Base:
 
     @classmethod
     @declared_attr
-    def __table_args__(cls) -> dict[str, str]:
+    def __table_args__(cls) -> dict[str, Any]:
         schema = f'{cls.__module__.split(".")[-2].upper()}_{cls.__module__.split(".")[-1].upper()}'
         add_table_schema_to_model(schema, Base.metadata)
 
-        return {'schema': schema, 'comment': cls.__doc__}
+        return {'schema': schema, 'comment': cls.__doc__, 'info': {'sensitive': False}}
 
     def __repr__(self) -> str:
         attrs = []
         for c in self.__table__.columns:
             attrs.append(f"{c.name}={getattr(self, c.name)}")
         return "{}({})".format(self.__class__.__name__, ", ".join(attrs))
+
+
+class SensitiveBase(Base):
+    """Base class for all sensitive entities"""
+
+    @classmethod
+    @declared_attr
+    def __table_args__(cls) -> dict[str, Any]:
+        schema = f'{cls.__module__.split(".")[-2].upper()}_{cls.__module__.split(".")[-1].upper()}'
+        add_table_schema_to_model(schema, Base.metadata)
+
+        return {'schema': schema, 'comment': cls.__doc__, 'info': {'sensitive': True}}
+
+    __abstract__ = True
+    __mapper_args__ = {'concrete': True}
