70bcd95add3d6f1f3c870871d2b1bda1dc1a6071
Захаров Иван Михайлович <zakharovim@it.profcomff.com>
2025-02-08 06:27:59 +0000

    fix schema

---
+++ migrations/versions/20250208_0626_cfbe67fec54d_update_usersesssion_schema.py
@@ -0,0 +1,27 @@
+"""update usersesssion_schema
+
+Revision ID: cfbe67fec54d
+Revises: f86ba4919842
+Create Date: 2025-02-08 06:26:16.969161
+
+"""
+
+import os
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = 'cfbe67fec54d'
+down_revision = 'f86ba4919842'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('user_session', sa.Column('is_unbounded', sa.Boolean(), nullable=True), schema='STG_AUTH')
+
+
+def downgrade():
+    op.drop_column('user_session', 'is_unbounded', schema='STG_AUTH')
