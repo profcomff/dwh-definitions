a87d09f00f6b6b813d27588bb0656e5e99566126
Захаров Иван Михайлович <zakharovim@it.profcomff.com>
2025-02-15 11:47:15 +0000

    init

---
+++ migrations/versions/20250215_1146_adb819726799_fix_stg_rating.py
@@ -0,0 +1,27 @@
+"""fix stg rating
+
+Revision ID: adb819726799
+Revises: cfbe67fec54d
+Create Date: 2025-02-15 11:46:07.544400
+
+"""
+
+import os
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = 'adb819726799'
+down_revision = 'cfbe67fec54d'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column('comment', 'subject', existing_type=sa.VARCHAR(), nullable=True, schema='STG_RATING')
+
+
+def downgrade():
+    op.alter_column('comment', 'subject', existing_type=sa.VARCHAR(), nullable=False, schema='STG_RATING')
