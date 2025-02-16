c559c673fd08dd578ac3f1b253fe8d38679fc12f
Mikhail <parfenov.ma21@physics.msu.ru>
2025-02-16 20:44:45 +0300

    init (#125)
    таблички из рейтинга, тяну выше

---
+++ migrations/versions/20250216_1740_41490fd26134_dwh_rating.py
@@ -0,0 +1,220 @@
+"""dwh_rating
+
+Revision ID: 41490fd26134
+Revises: 8a7cb1960f13
+Create Date: 2025-02-16 17:40:38.011446
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import os
+
+
+# revision identifiers, used by Alembic.
+revision = '41490fd26134'
+down_revision = '8a7cb1960f13'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table_schema("DWH_RATING")
+    op.create_table(
+        'comment',
+        sa.Column('uuid', sa.Uuid(), nullable=False, comment='Техническое поле в dwh'),
+        sa.Column('api_uuid', sa.Uuid(), nullable=False, comment='Идентифиактор в rating-api'),
+        sa.Column('create_ts', sa.DateTime(), nullable=False, comment='Timestamp создания комментария, мск'),
+        sa.Column('update_ts', sa.DateTime(), nullable=False, comment='Timestamp обновления комментария, мск'),
+        sa.Column('subject', sa.String(), nullable=True, comment='Предмет, к которому относится комментарий'),
+        sa.Column('text', sa.String(), nullable=True, comment='Текст комментария'),
+        sa.Column('mark_kindness', sa.Integer(), nullable=False, comment='Доброта преподавателя'),
+        sa.Column('mark_freebie', sa.Integer(), nullable=False, comment='Халявность преподавателя'),
+        sa.Column('mark_clarity', sa.Integer(), nullable=False, comment='Понятность преподавателя'),
+        sa.Column('lecturer_id', sa.Integer(), nullable=False, comment='Идертификатор преподавателя'),
+        sa.Column(
+            'review_status',
+            sa.String(),
+            nullable=False,
+            comment='Статус комментария, может быть approved, pending, dismissed',
+        ),
+        sa.Column('valid_from_dt', sa.Date(), nullable=True, comment='Дата начала действия записи'),
+        sa.Column('valid_to_dt', sa.Date(), nullable=True, comment='Дата конца действия записи'),
+        sa.PrimaryKeyConstraint('uuid'),
+        schema='DWH_RATING',
+        comment='\n    Комментарии в rating-api\n    ',
+        info={'sensitive': False},
+    )
+    op.create_table(
+        'lecturer',
+        sa.Column('uuid', sa.Uuid(), nullable=False, comment='Техническое поле в dwh'),
+        sa.Column('api_id', sa.Integer(), nullable=False, comment='Идентифиактор в rating-api'),
+        sa.Column('first_name', sa.String(), nullable=False, comment='Имя преподавателя'),
+        sa.Column('last_name', sa.String(), nullable=False, comment='Фамилия преподавателя'),
+        sa.Column('middle_name', sa.String(), nullable=False, comment='отчество преподавателя'),
+        sa.Column('subject', sa.String(), nullable=True, comment='Список предметов преподавателя'),
+        sa.Column('avatar_link', sa.String(), nullable=True, comment='Ссылка на аватар преподавателя'),
+        sa.Column('timetable_id', sa.Integer(), nullable=False, comment='Идертификатор в timetable-api'),
+        sa.Column('valid_from_dt', sa.Date(), nullable=True, comment='Дата начала действия записи'),
+        sa.Column('valid_to_dt', sa.Date(), nullable=True, comment='Дата конца действия записи'),
+        sa.PrimaryKeyConstraint('uuid'),
+        schema='DWH_RATING',
+        comment='\n    Преподаватели в rating-api\n    ',
+        info={'sensitive': False},
+    )
+    op.create_table(
+        'lecturer_user_comment',
+        sa.Column('uuid', sa.Uuid(), nullable=False, comment='Техническое поле в dwh'),
+        sa.Column('api_id', sa.Integer(), nullable=False, comment='Идентифиактор в rating-api'),
+        sa.Column('lecturer_id', sa.Integer(), nullable=False, comment='Идентифиактор преподавателя'),
+        sa.Column('user_id', sa.Integer(), nullable=False, comment='Идентификатор пользователя в auth-api'),
+        sa.Column('create_ts', sa.DateTime(), nullable=False, comment='Timestamp создания комментария, мск'),
+        sa.Column('update_ts', sa.DateTime(), nullable=False, comment='Timestamp обновления комментария, мск'),
+        sa.Column('valid_from_dt', sa.Date(), nullable=True, comment='Дата начала действия записи'),
+        sa.Column('valid_to_dt', sa.Date(), nullable=True, comment='Дата конца действия записи'),
+        sa.PrimaryKeyConstraint('uuid'),
+        schema='DWH_RATING',
+        comment='\n    Связь лекторов и комметариев в rating-api\n    ',
+        info={'sensitive': False},
+    )
+    op.create_group(
+        "test_dwh_dwh_rating_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_read"
+    )
+    op.create_group(
+        "test_dwh_dwh_rating_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_write"
+    )
+    op.create_group(
+        "test_dwh_dwh_rating_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_all"
+    )
+    op.grant_on_schema(
+        "test_dwh_dwh_rating_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_read",
+        "DWH_RATING",
+    )
+    op.grant_on_schema(
+        "test_dwh_dwh_rating_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_write",
+        "DWH_RATING",
+    )
+    op.grant_on_schema(
+        "test_dwh_dwh_rating_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_all",
+        "DWH_RATING",
+    )
+    op.grant_on_table(
+        "test_dwh_dwh_rating_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_read",
+        ['SELECT'],
+        '"DWH_RATING".comment',
+    )
+    op.grant_on_table(
+        "test_dwh_dwh_rating_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_write",
+        ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
+        '"DWH_RATING".comment',
+    )
+    op.grant_on_table(
+        "test_dwh_dwh_rating_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_all",
+        ['ALL'],
+        '"DWH_RATING".comment',
+    )
+    op.grant_on_table(
+        "test_dwh_dwh_rating_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_read",
+        ['SELECT'],
+        '"DWH_RATING".lecturer_user_comment',
+    )
+    op.grant_on_table(
+        "test_dwh_dwh_rating_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_write",
+        ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
+        '"DWH_RATING".lecturer_user_comment',
+    )
+    op.grant_on_table(
+        "test_dwh_dwh_rating_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_all",
+        ['ALL'],
+        '"DWH_RATING".lecturer_user_comment',
+    )
+    op.grant_on_table(
+        "test_dwh_dwh_rating_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_read",
+        ['SELECT'],
+        '"DWH_RATING".lecturer',
+    )
+    op.grant_on_table(
+        "test_dwh_dwh_rating_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_write",
+        ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
+        '"DWH_RATING".lecturer',
+    )
+    op.grant_on_table(
+        "test_dwh_dwh_rating_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_all",
+        ['ALL'],
+        '"DWH_RATING".lecturer',
+    )
+    # ### end Alembic commands ###
+
+
+def downgrade():
+    # ### commands auto generated by Alembic - please adjust! ###
+    op.revoke_on_table(
+        "test_dwh_dwh_rating_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_all",
+        ['ALL'],
+        '"DWH_RATING".lecturer',
+    )
+    op.revoke_on_table(
+        "test_dwh_dwh_rating_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_write",
+        ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
+        '"DWH_RATING".lecturer',
+    )
+    op.revoke_on_table(
+        "test_dwh_dwh_rating_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_read",
+        ['SELECT'],
+        '"DWH_RATING".lecturer',
+    )
+    op.revoke_on_table(
+        "test_dwh_dwh_rating_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_all",
+        ['ALL'],
+        '"DWH_RATING".lecturer_user_comment',
+    )
+    op.revoke_on_table(
+        "test_dwh_dwh_rating_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_write",
+        ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
+        '"DWH_RATING".lecturer_user_comment',
+    )
+    op.revoke_on_table(
+        "test_dwh_dwh_rating_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_read",
+        ['SELECT'],
+        '"DWH_RATING".lecturer_user_comment',
+    )
+    op.revoke_on_table(
+        "test_dwh_dwh_rating_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_all",
+        ['ALL'],
+        '"DWH_RATING".comment',
+    )
+    op.revoke_on_table(
+        "test_dwh_dwh_rating_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_write",
+        ['SELECT', 'UPDATE', 'DELETE', 'TRUNCATE', 'INSERT'],
+        '"DWH_RATING".comment',
+    )
+    op.revoke_on_table(
+        "test_dwh_dwh_rating_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_read",
+        ['SELECT'],
+        '"DWH_RATING".comment',
+    )
+    op.revoke_on_schema(
+        "test_dwh_dwh_rating_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_all",
+        "DWH_RATING",
+    )
+    op.revoke_on_schema(
+        "test_dwh_dwh_rating_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_write",
+        "DWH_RATING",
+    )
+    op.revoke_on_schema(
+        "test_dwh_dwh_rating_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_read",
+        "DWH_RATING",
+    )
+    op.drop_table('lecturer_user_comment', schema='DWH_RATING')
+    op.drop_table('lecturer', schema='DWH_RATING')
+    op.drop_table('comment', schema='DWH_RATING')
+    op.delete_group(
+        "test_dwh_dwh_rating_all" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_all"
+    )
+    op.delete_group(
+        "test_dwh_dwh_rating_write" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_write"
+    )
+    op.delete_group(
+        "test_dwh_dwh_rating_read" if os.getenv("ENVIRONMENT") != "production" else "prod_dwh_dwh_rating_read"
+    )
+    op.drop_table_schema("DWH_RATING")
---
+++ profcomff_definitions/ODS/rating.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from uuid import UUID
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from profcomff_definitions.base import Base
+
+
+class Lecturer(Base):
+    """
+    Преподаватели в rating-api
+    """
+    uuid: Mapped[UUID] = mapped_column(primary_key=True, comment="Техническое поле в dwh")
+    api_id: Mapped[int] = mapped_column(comment="Идентифиактор в rating-api")
+    first_name: Mapped[str] = mapped_column(comment="Имя преподавателя")
+    last_name: Mapped[str] = mapped_column(comment="Фамилия преподавателя")
+    middle_name: Mapped[str] = mapped_column(comment="отчество преподавателя")
+    subject: Mapped[str | None] = mapped_column(comment="Список предметов преподавателя")
+    avatar_link: Mapped[str | None] = mapped_column(comment="Ссылка на аватар преподавателя")
+    timetable_id: Mapped[int] = mapped_column(comment="Идертификатор в timetable-api")
+
+
+class Comment(Base):
+    """
+    Комментарии в rating-api
+    """
+    uuid: Mapped[UUID] = mapped_column(primary_key=True, comment="Техническое поле в dwh")
+    api_uuid: Mapped[UUID] = mapped_column(comment="Идентифиактор в rating-api")
+    create_ts: Mapped[datetime] = mapped_column(comment="Timestamp создания комментария, мск")
+    update_ts: Mapped[datetime] = mapped_column(comment="Timestamp обновления комментария, мск")
+    subject: Mapped[str | None] = mapped_column(comment="Предмет, к которому относится комментарий")
+    text: Mapped[str | None] = mapped_column(comment="Текст комментария")
+    mark_kindness: Mapped[int] = mapped_column(comment="Доброта преподавателя")
+    mark_freebie: Mapped[int] = mapped_column(comment="Халявность преподавателя")
+    mark_clarity: Mapped[int] = mapped_column(comment="Понятность преподавателя")
+    lecturer_id: Mapped[int] = mapped_column(comment="Идертификатор преподавателя")
+    review_status: Mapped[str] = mapped_column(comment="Статус комментария, может быть approved, pending, dismissed")
+
+
+class LecturerUserComment(Base):
+    """
+    Связь лекторов и комметариев в rating-api
+    """
+    uuid: Mapped[UUID] = mapped_column(primary_key=True, comment="Техническое поле в dwh")
+    api_id: Mapped[int] = mapped_column(comment="Идентифиактор в rating-api")
+    lecturer_id: Mapped[int] = mapped_column(comment="Идентифиактор преподавателя")
+    user_id: Mapped[int] = mapped_column(comment="Идентификатор пользователя в auth-api")
+    create_ts: Mapped[datetime] = mapped_column(comment="Timestamp создания комментария, мск")
+    update_ts: Mapped[datetime] = mapped_column(comment="Timestamp обновления комментария, мск")
