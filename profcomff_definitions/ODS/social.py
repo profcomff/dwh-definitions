51e22fcad3e03c6426783abdd7b9b0b23af4a012
Mikhail <parfenov.ma21@physics.msu.ru>
2025-04-16 00:58:22 +0300

    Add soc (#140)

--- profcomff_definitions/ODS/social.py
+++ profcomff_definitions/ODS/social.py
@@ -6,13 +6,13 @@ from sqlalchemy.orm import Mapped, mapped_column
 from profcomff_definitions.base import Base
 
 
-class Lecturer(Base):
+class ViribusChat(Base):
     """
     Преподаватели в rating-api
     """
 
     uuid: Mapped[UUID] = mapped_column(primary_key=True, comment="Техническое поле в dwh")
-    thread_name: Mapped[str] = mapped_column(comment="Название ветки в сообществе, например Frontend")
-    message_text: Mapped[str] = mapped_column(comment="Текст сообщения в телеграме")
-    sender_telegram_login: Mapped[str] = mapped_column(comment="Логин пользователя в телеграме")
+    thread_name: Mapped[str | None] = mapped_column(comment="Название ветки в сообществе, например Frontend")
+    message_text: Mapped[str | None] = mapped_column(comment="Текст сообщения в телеграме")
+    sender_telegram_login: Mapped[str | None] = mapped_column(comment="Логин пользователя в телеграме")
     message_ts: Mapped[datetime] = mapped_column(comment="Таймстемп записи в social-api")
