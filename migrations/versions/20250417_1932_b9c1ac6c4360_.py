84e3c67fc1fc4bdf975e43e80e74b3c200b6bcfb
parfenovma <parfenov.ma21@physics.msu.ru>
2025-04-17 19:35:28 +0000

    add col in marketing

---
+++ migrations/versions/20250417_1932_b9c1ac6c4360_.py
@@ -0,0 +1,26 @@
+"""empty message
+
+Revision ID: b9c1ac6c4360
+Revises: 5c9e53fd0e21, 40d72f2dd950
+Create Date: 2025-04-17 19:32:27.487669
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import os
+
+
+# revision identifiers, used by Alembic.
+revision = 'b9c1ac6c4360'
+down_revision = ('5c9e53fd0e21', '40d72f2dd950')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/20250417_1934_018159bb0cfc_add_col_in_dm_marketing.py
@@ -0,0 +1,52 @@
+"""add col in dm_marketing
+
+Revision ID: 018159bb0cfc
+Revises: b9c1ac6c4360
+Create Date: 2025-04-17 19:34:42.627766
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+import os
+
+
+# revision identifiers, used by Alembic.
+revision = '018159bb0cfc'
+down_revision = 'b9c1ac6c4360'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        'frontend_actions_services',
+        sa.Column(
+            'service_from_name', sa.String(), nullable=True, comment='Назввание сервиса, откуда пользователь перешел'
+        ),
+        schema='DM_MARKETING',
+    )
+    op.add_column(
+        'frontend_actions_services',
+        sa.Column(
+            'service_to_name', sa.String(), nullable=True, comment='Назввание сервиса, куда пользователь перешел'
+        ),
+        schema='DM_MARKETING',
+    )
+    op.drop_column('frontend_actions_services', 'service_name', schema='DM_MARKETING')
+
+
+def downgrade():
+    op.add_column(
+        'frontend_actions_services',
+        sa.Column(
+            'service_name',
+            sa.VARCHAR(),
+            autoincrement=False,
+            nullable=False,
+            comment='Назввание сервиса, куда пользователь перешел',
+        ),
+        schema='DM_MARKETING',
+    )
+    op.drop_column('frontend_actions_services', 'service_to_name', schema='DM_MARKETING')
+    op.drop_column('frontend_actions_services', 'service_from_name', schema='DM_MARKETING')
