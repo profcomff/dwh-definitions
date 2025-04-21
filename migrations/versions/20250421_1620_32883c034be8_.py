afad4c8a4404e6cef276ae788e1d1d25be6a86a1
parfenovma <parfenov.ma21@physics.msu.ru>
2025-04-21 16:22:19 +0000

    fix migrations

---
+++ migrations/versions/20250421_1620_32883c034be8_.py
@@ -0,0 +1,27 @@
+"""empty message
+
+Revision ID: 32883c034be8
+Revises: d43d62fb3748, 018159bb0cfc
+Create Date: 2025-04-21 16:20:54.982505
+
+"""
+
+import os
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '32883c034be8'
+down_revision = ('d43d62fb3748', '018159bb0cfc')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
