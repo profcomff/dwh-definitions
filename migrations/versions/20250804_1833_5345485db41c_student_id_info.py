b4ac3ba9d7e016b5b005abcf44a9f954c4416d4d
Vladislav <2007.5v.voskoboinik.vladislav@gmail.com>
2025-08-04 18:45:28 +0300

    student-id

---
+++ migrations/versions/20250804_1833_5345485db41c_student_id_info.py
@@ -0,0 +1,28 @@
+"""student-id-info
+
+Revision ID: 5345485db41c
+Revises: 80e114fe1399
+Create Date: 2025-08-04 18:33:51.347044
+
+"""
+
+import os
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = '5345485db41c'
+down_revision = '80e114fe1399'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('union_member', sa.Column('student_id', sa.String(), nullable=True), schema='STG_UNION_MEMBER')
+
+
+def downgrade():
+    op.drop_column('union_member', 'student_id', schema='STG_UNION_MEMBER')
+    
