06c094ca760b4f61d0e233985e070f8eec58cf4d
Vladislav <86772772+VladislavVoskoboinik@users.noreply.github.com>
2025-09-19 22:02:07 +0300

    витрины рентала (#170)
    ## Изменения
    <!-- Опишите здесь на языке, понятном каждому, изменения, сделанные в
    исходном коде по пунктам. -->
    Рефактор витрины с арендами + добавлена витрина страйков рентала на
    dm_rental
    ## Детали реализации
    <!-- Здесь можно описать технические детали по пунктам. -->
    
    ## Check-List
    <!-- После сохранения у следующих полей появятся галочки, которые нужно
    проставить мышкой -->
    - [ ] Вы проверили свой код перед отправкой запроса?
    - [ ] Вы написали тесты к реализованным функциям?
    - [ ] Вы не забыли применить форматирование `black` и `isort` для
    _Back-End_ или `Prettier` для _Front-End_?
    
    Co-authored-by: VladislavV <vladislavv@VladislavVs-MacBook-Air.local>

---
+++ migrations/versions/20250919_2126_3ac342622c64_dm_rental_refactor_and_strike_datamart.py
@@ -0,0 +1,105 @@
+"""dm_rental_refactor_and_strike_datamart
+
+Revision ID: 3ac342622c64
+Revises: 1a383420f47f
+Create Date: 2025-09-19 21:26:39.203543
+
+"""
+
+import os
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy.dialects import postgresql
+
+
+revision = '3ac342622c64'
+down_revision = '1a383420f47f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        'dm_strike',
+        sa.Column('strike_id', sa.Integer(), nullable=False, comment='Идентификатор страйка'),
+        sa.Column('user_id', sa.Integer(), nullable=False, comment='Идентификатор пользователя'),
+        sa.Column('user_segment', sa.String(), nullable=True, comment='Сегмент пользователя (опционально)'),
+        sa.Column('session_id', sa.Integer(), nullable=False, comment='Идентификатор сессии аренды'),
+        sa.Column('item_id', sa.Integer(), nullable=False, comment='Идентификатор вещи'),
+        sa.Column('item_type_id', sa.Integer(), nullable=False, comment='Идентификатор типа вещи'),
+        sa.Column('item_type_name', sa.String(), nullable=True, comment='Название типа вещи'),
+        sa.Column('item_name', sa.String(), nullable=False, comment='Название вещи'),
+        sa.Column('strike_reason', sa.String(), nullable=True, comment='Причина страйка'),
+        sa.Column('strike_date', sa.DateTime(), nullable=False, comment='Дата и время начисления страйка'),
+        sa.Column('admin_id', sa.Integer(), nullable=False, comment='Идентификатор администратора, вынесшего страйк'),
+        sa.Column('admin_name', sa.String(), nullable=True, comment='Имя администратора (де-normalized)'),
+        sa.Column('total_strikes_user', sa.Integer(), nullable=False, comment='Общее количество страйков пользователя'),
+        sa.Column('total_strikes_session', sa.Integer(), nullable=False, comment='Общее количество страйков в сессии'),
+        sa.Column('rental_start_ts', sa.DateTime(), nullable=False, comment='Начало аренды'),
+        sa.Column('rental_end_ts', sa.DateTime(), nullable=False, comment='Планируемое окончание аренды'),
+        sa.Column('return_ts', sa.DateTime(), nullable=True, comment='Фактическое время возврата'),
+        sa.Column('session_status', sa.String(), nullable=True, comment='Статус сессии аренды'),
+        sa.PrimaryKeyConstraint('strike_id'),
+        schema='DM_RENTAL',
+        comment='\n    Витрина страйки\n    ',
+        info={'sensitive': False},
+    )
+    op.add_column(
+        'dm_rentals_events',
+        sa.Column('session_duration_minutes', sa.DateTime(), nullable=True, comment='Длительность аренды в мин'),
+        schema='DM_RENTAL',
+    )
+    op.add_column(
+        'dm_rentals_events',
+        sa.Column('overdue_minutes', sa.DateTime(), nullable=True, comment='Просрочка времени аренды в мин'),
+        schema='DM_RENTAL',
+    )
+    op.add_column(
+        'dm_rentals_events',
+        sa.Column('strikes_count', sa.Integer(), nullable=False, comment='Количество страйков за сессию'),
+        schema='DM_RENTAL',
+    )
+    op.create_table_comment(
+        'dm_rentals_events',
+        '\n    Витрина данных арендных сессий\n    ',
+        existing_comment='\n    Данные арендных сессий\n    ',
+        schema='DM_RENTAL',
+    )
+    op.drop_column('dm_rentals_events', 'strike_date', schema='DM_RENTAL')
+    op.drop_column('dm_rentals_events', 'strike_reason', schema='DM_RENTAL')
+    op.drop_column('dm_rentals_events', 'admin_strike_id', schema='DM_RENTAL')
+
+
+def downgrade():
+    op.add_column(
+        'dm_rentals_events',
+        sa.Column('admin_strike_id', sa.INTEGER(), autoincrement=False, nullable=True, comment='Идентификаор админа'),
+        schema='DM_RENTAL',
+    )
+    op.add_column(
+        'dm_rentals_events',
+        sa.Column('strike_reason', sa.VARCHAR(), autoincrement=False, nullable=True, comment='Причина страйка'),
+        schema='DM_RENTAL',
+    )
+    op.add_column(
+        'dm_rentals_events',
+        sa.Column(
+            'strike_date',
+            postgresql.TIMESTAMP(),
+            autoincrement=False,
+            nullable=True,
+            comment='Timestamp начисления страйка, мск',
+        ),
+        schema='DM_RENTAL',
+    )
+    op.create_table_comment(
+        'dm_rentals_events',
+        '\n    Данные арендных сессий\n    ',
+        existing_comment='\n    Витрина данных арендных сессий\n    ',
+        schema='DM_RENTAL',
+    )
+    op.drop_column('dm_rentals_events', 'strikes_count', schema='DM_RENTAL')
+    op.drop_column('dm_rentals_events', 'overdue_minutes', schema='DM_RENTAL')
+    op.drop_column('dm_rentals_events', 'session_duration_minutes', schema='DM_RENTAL')
+    op.drop_table('dm_strike', schema='DM_RENTAL')
--- profcomff_definitions/DM/rental.py
+++ profcomff_definitions/DM/rental.py
@@ -8,7 +8,7 @@ from profcomff_definitions.base import Base
 
 class DmRentalsEvents(Base):
     """
-    Данные арендных сессий
+    Витрина данных арендных сессий
     """
 
     uuid: Mapped[UUID] = mapped_column(primary_key=True, comment="Техническое поле dwh")
@@ -26,6 +26,32 @@ class DmRentalsEvents(Base):
     image_url: Mapped[str | None] = mapped_column(comment="Ссылка на фото вещи")
     description: Mapped[str | None] = mapped_column(comment="Описание вещи")
     session_id: Mapped[int] = mapped_column(comment="Идентификатор сессии")
-    admin_strike_id: Mapped[int | None] = mapped_column(comment="Идентификаор админа")
+    session_duration_minutes: Mapped[datetime | None] = mapped_column(comment="Длительность аренды в мин")
+    overdue_minutes: Mapped[datetime | None] = mapped_column(comment="Просрочка времени аренды в мин")
+    strikes_count: Mapped[int] = mapped_column(comment="Количество страйков за сессию")
+
+
+class DmStrike(Base):
+    __tablename__ = "dm_strike"
+    """
+    Витрина страйки
+    """
+
+    strike_id: Mapped[int] = mapped_column(primary_key=True, comment="Идентификатор страйка")
+    user_id: Mapped[int] = mapped_column(comment="Идентификатор пользователя")
+    user_segment: Mapped[str | None] = mapped_column(comment="Сегмент пользователя (опционально)")
+    session_id: Mapped[int] = mapped_column(comment="Идентификатор сессии аренды")
+    item_id: Mapped[int] = mapped_column(comment="Идентификатор вещи")
+    item_type_id: Mapped[int] = mapped_column(comment="Идентификатор типа вещи")
+    item_type_name: Mapped[str | None] = mapped_column(comment="Название типа вещи")
+    item_name: Mapped[str] = mapped_column(comment="Название вещи")
     strike_reason: Mapped[str | None] = mapped_column(comment="Причина страйка")
-    strike_date: Mapped[datetime | None] = mapped_column(comment="Timestamp начисления страйка, мск")
+    strike_date: Mapped[datetime] = mapped_column(comment="Дата и время начисления страйка")
+    admin_id: Mapped[int] = mapped_column(comment="Идентификатор администратора, вынесшего страйк")
+    admin_name: Mapped[str | None] = mapped_column(comment="Имя администратора (де-normalized)")
+    total_strikes_user: Mapped[int] = mapped_column(comment="Общее количество страйков пользователя")
+    total_strikes_session: Mapped[int] = mapped_column(comment="Общее количество страйков в сессии")
+    rental_start_ts: Mapped[datetime] = mapped_column(comment="Начало аренды")
+    rental_end_ts: Mapped[datetime] = mapped_column(comment="Планируемое окончание аренды")
+    return_ts: Mapped[datetime | None] = mapped_column(comment="Фактическое время возврата")
+    session_status: Mapped[str | None] = mapped_column(comment="Статус сессии аренды")
