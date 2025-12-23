ffe95ae64bafcdbb2eb07942703b98218b05629a
mihailunusov <unusovmisa3@gmail.com>
2025-12-23 20:35:11 +0300

    add migrations

---
+++ migrations/versions/20251222_1641_d5063f55a1f4_merge_all_heads.py
@@ -0,0 +1,27 @@
+"""merge all heads
+
+Revision ID: d5063f55a1f4
+Revises: a0d420b720ba, d27b68da6902
+Create Date: 2025-12-22 16:41:43.121271
+
+"""
+
+import os
+
+import sqlalchemy as sa
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision = 'd5063f55a1f4'
+down_revision = ('a0d420b720ba', 'd27b68da6902')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
